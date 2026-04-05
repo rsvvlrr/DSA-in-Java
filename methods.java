586f93c150ee00ab930c610b26c688724f4f3e44
Raksha Shah <rs.vvlr.rs@gmail.com>
2026-04-05 13:30:18 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class methods{
+
+public static void raksha(){
+System.out.println("rs");
+}
+public static void main(String [] args){
+raksha();
+riyansh();
+dubadubai();
+System.out.println("raksha shah");
+}
+
+public static void riyansh(){
+raksha();
+System.out.println("dubadubai");
+}
+public static void dubadubai(){
+riyansh();
+raksha();
+System.out.println("chadrmukhi chotala");
+}
+}
