8e629d7f66aa078c0c0475397e3419617ec2a923
Raksha Shah <rs.vvlr.rs@gmail.com>
2026-04-05 13:29:25 +0530

    Add files via upload

---
+++ argumnet.java
@@ -0,0 +1,12 @@
+public class argumnet {
+   
+    public static void sum(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+            System.out.println("Hello");
+        sum(5,8);
+        System.out.println();
+    }
+}
+
